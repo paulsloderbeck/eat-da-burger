044bf01053c72c7acb22c9eaa16c8d330657ac72
paulsloderbeck <64672063+paulsloderbeck@users.noreply.github.com>
2020-08-15 19:54:00 -0400

    Added jawsdb if else statement in connection for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 require("dotenv").config();
-
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.DB_PASS,
-    database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
+}
 
 // Make connection.
 connection.connect(function (err) {
--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,9 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name varchar(255) NOT NULL,
+    id int NOT NULL
+    AUTO_INCREMENT PRIMARY KEY,
+    name varchar
+    (255) NOT NULL,
     devoured BOOLEAN DEFAULT false
 );
